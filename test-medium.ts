b857b72b99c42e4404da50ebfdcbc029c3121021
DavinciDreams <lisamegawatts@gmail.com>
2025-07-06 07:13:05 -0400

    tests

---
+++ test-medium.ts
@@ -0,0 +1,20 @@
+// Medium complexity file to test AST mode
+var count = 0;
+var active = true;
+
+function simpleFunction(input: string): string {
+  if (input == 'test') {
+    return 'tested';
+  }
+  return input;
+}
+
+const items = [1, 2, 3];
+for (var i = 0; i < items.length; i++) {
+  if (items[i] == 2) {
+    console.log('Error: Found item 2');
+  }
+}
+
+var result = simpleFunction('hello');
+console.log(result);
