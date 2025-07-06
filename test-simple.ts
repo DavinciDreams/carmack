d58e99ad51d6629593bc04fbcfc1b17f824d039e
DavinciDreams <lisamegawatts@gmail.com>
2025-07-06 11:37:36 -0400

    simple test

---
+++ test-simple.ts
@@ -0,0 +1,10 @@
+// Simple file to test var transformations
+var userName = 'John';
+var userAge = 25;
+var isActiveUser = true;
+
+function test() {
+  if (userName === 'John') {
+    console.log('Error: Found John');
+  }
+}
