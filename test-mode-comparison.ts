c7f603baa68553af5bcec1c61b0ef6552647bcf2
DavinciDreams <lisamegawatts@gmail.com>
2025-07-06 21:23:07 -0400

    Pre-transformation checkpoint - 6b721238-6892-4385-921e-ba87edc43ea6

---
+++ test-mode-comparison.ts
@@ -0,0 +1,8 @@
+// Test file to compare template vs AST transformations
+var clickCount = 0;
+var isEnabled = true;
+
+function exampleFunction() {
+  // String concatenation  
+  const msg = 'Welcome ' + username + ' to the app';
+}
