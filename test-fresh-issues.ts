24dc910c73e51058bba0bc5a3f86f2290f71fec4
DavinciDreams <lisamegawatts@gmail.com>
2025-07-06 20:01:22 -0400

    Transformation 8eefcc1e-a88d-4a0a-b88f-36038ba1c6ec completed with template mode

--- test-fresh-issues.ts
+++ test-fresh-issues.ts
@@ -1,14 +1,14 @@
 // Test file with var issues that the system should fix
-var appCounter = 0;
-var systemReady = false;
-var currentUser = 'default';
+const appCounter = 0;;
+const systemReady = false;;
+const currentUser = 'default';;
 
 function processData() {
-  var localData = 'test';
-  var dataCount = 0;
+  const localData = 'test';;
+  const dataCount = 0;;
   
   for (var i = 0; i < 10; i++) {
-    if (localData == 'test') {
+    if (localData === 'test') {
       dataCount++;
     }
   }
@@ -17,13 +17,13 @@ function processData() {
 }
 
 // Some equality issues to fix  
-if (currentUser == 'admin') {
+if (currentUser === 'admin') {
   console.log('Admin user');
 }
 
-var dataItems = [1, 2, 3];
+const dataItems = [1, 2, 3];;
 for (var j = 0; j < dataItems.length; j++) {
-  if (dataItems[j] == 2) {
+  if (dataItems[j] === 2) {
     console.log('Found 2');
   }
 }
