89bfba55a68bd948c6fc7e8c30f3c25be4129165
DavinciDreams <lisamegawatts@gmail.com>
2025-07-15 03:29:28 -0400

    delete extra tests

--- temp-test.ts
+++
@@ -1,6 +0,0 @@
-const double = (x) => {
-  return x * 2;
-};
-const add = (a, b) => {
-  return a + b;
-};
--- test-mode-comparison.ts
+++
@@ -1,7 +0,0 @@
-// Test file to compare template vs AST transformations
-const clickCount = 0;
-const isEnabled = true;
-function exampleFunction() {
-  // String concatenation
-  const msg = 'Welcome ' + username + ' to the app';
-}
