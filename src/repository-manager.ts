1ad3c4b5a23871582b53ce89e098a504f7cdfc05
DavinciDreams <lisamegawatts@gmail.com>
2025-07-26 19:23:24 -0400

    repo manager

--- src/repository-manager.ts
+++ src/repository-manager.ts
@@ -1,10 +1,12 @@
 import { createActor, fromPromise } from 'xstate';
 import { z } from 'zod';
+
 import { gitActor } from './actors/git.js';
 import { type LearningResult, patternLearningActor } from './actors/pattern-learning.js';
 import { carmackCoderMachine } from './machine.js';
-import type { AstPattern, TransformationRequest, TransformationResult } from './types.js';
 import { AstPatternSchema, TransformationRequestSchema } from './types.js';
+
+import type { AstPattern, TransformationRequest, TransformationResult } from './types.js';
 
 // Repository management types and schemas
 export const RepositoryConfigSchema = z.object({
@@ -1117,4 +1119,4 @@ export const processRepository = async (
 
 export const validateRepositoryConfig = (data: unknown): RepositoryConfig => {
   return RepositoryConfigSchema.parse(data);
-};
+};
